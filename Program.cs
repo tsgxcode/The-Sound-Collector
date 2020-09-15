dffca39c7f843bc5ab535a326f98e1ac1a11d0de
TSG <TSG@TSGAC>
2020-09-15 06:40:03 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TheSoundCollector
+{
+    class Program
+    {
+        static void Main()
+        {
+            /* The Sound Colloctor - exploring the world of sound.
+          A library of found sounds and the tools used to find them.
+        */
+            string author = "T. Scott George";
+            string appName = "The Sound Collector";
+            string appVersion = "1.0";
+
+
+            Console.WriteLine("{0} {1} {2}", author, appName, appVersion );
+            
+            //Making it pretty :)
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Collecting Sounds From Everywhere! ");
+
+            Console.ResetColor();
+
+
+
+        }
+    }
+}
